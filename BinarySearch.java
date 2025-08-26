745a02d9ffdd6f5cdc79e94031b89e70bfe6ba15
Parisharma03 <sharmapari0522@gmail.com>
2025-08-26 21:08:55 +0530

    BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,3 +1,31 @@
-public class BinarySearch{
-  
+public class BinarySearch {
+    public static void main(String[] args) {
+
+        int[] arr = {1,3,4,5,6,8,9};
+        int target = 1;
+        int s = 0;
+        int e = arr.length - 1;
+        int res = binary(arr, s, e, target);
+        System.out.println("the target is at index :"+res);
+    }
+
+    static int binary(int[] arr,int start,int end,int target) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target == arr[mid]) {
+                return mid;
+            }
+            if (target < arr[mid]) {
+                end = mid - 1;
+
+            } else {
+                start = mid + 1;
+            }
+
+
+        }
+        return -1;
+    }
+
 }
+
